93af2f453f17af790ee37e7bee1ec483097bde96
ichiz7 <ichibakry7@gmail.com>
2024-06-06 19:15:50 +0200

    add line class

--- graphics.py
+++ graphics.py
@@ -28,3 +28,15 @@ class Point:
         self.x = x
         self.y = y
 
+class Line:
+    def __init__(self, start_point, end_point):
+        self.start_point = start_point
+        self.end_point = end_point
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.start_point.x, self.start_point.y,
+            self.end_point.x, self.end_point.y, 
+            fill = fill_color, width = 2
+            )
+        
